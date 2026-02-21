ed79987e412d2e8bdf45b42f665c41ed55be0d4c
ryan2625 <ryancfreas@gmail.com>
2026-02-20 22:30:26 -0600

    Testing out typdef with vectors

---
+++ test2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+typedef std::vector<std::pair<std::string, int>> pairlist_t;
+
+int main () {
+    pairlist_t pairs;
+    std::cout << "Hi";
+    return 0;
+}
